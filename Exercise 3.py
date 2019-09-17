7b73cebc28c09bd3918da42149fbb1a03a0996e6
camp0851 <camp0851@algonquinlive.com>
2019-09-17 16:18:49 -0400

    updated exercise 3.py

--- Exercise 3.py
+++ Exercise 3.py
@@ -1,1 +1,13 @@
- currentTime = input
+#GET INPUTS
+currentHour = input('What hour is it right now? ')
+currentHour = int(currentHour)
+alarm =input('In how many hours do you want the alarm to go off? ')
+alarm = int(alarm)
+
+#
+totalHours = alarm + currentHour
+timeRemaining = totalHours % 24
+
+#print('totalHours: ' + str(totalHours))
+#print('hoursRemaining: ' + str(hoursRemaing))
+print('The alarm will go off at ' + str(timeRemaining))
